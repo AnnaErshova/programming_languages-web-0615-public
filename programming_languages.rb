e27e68788264d10d5ae499df07431268ef67e6f2
AnnaErshova <anna.ershova@gmail.com>
2015-06-12 13:43:31 -0400

    all done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,62 @@
+=begin
+  
+rescue Exception => e
+  
+endlanguages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+=end
+
+# THIS IS INCOMPLETE
+
 def reformat_languages(languages)
-  # your code here
-end
+  new_hash = {}
+  languages.each do |style, value| # => key = style; value = other
+    value.each do |language, type| # => type is still a key-value pair
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = type
+        new_hash[language][:style] = [style]
+      end # end if statement
+    end # end value block
+  end # end language block
+  new_hash
+end # end method
+
+
+# original hash:
+# {style => { language_name => { :type => value}}}
+# need to have:
+# {languages => {style => value, :type => value}}}
+
+
+
